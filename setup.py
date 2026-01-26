ed36faf248392d138b8b9134249b4339f1f38e96
Bibhuti dora <dsurekha08@gmail.com>
2026-01-26 10:01:47 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DASH = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    """This function will return the list of requirements"""
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DASH in requirements:
+            requirements.remove(HYPEN_E_DASH)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Bibhuti",
+    author_email="dsurekha08@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirement.txt")
+
+)
